4d7e3c49b76c7d8cc7f2eec1a309ed9dd1d1654c
saragolestani <sarah.gln.97@gmail.com>
2022-07-03 21:49:30 -0600

    close annot file after reading json

--- helper_functions/pycocotools/coco.py
+++ helper_functions/pycocotools/coco.py
@@ -75,8 +75,10 @@ class COCO:
         if not annotation_file == None:
             print('loading annotations into memory...')
             tic = time.time()
-            dataset = json.load(open(annotation_file, 'r'))
-            assert type(dataset)==dict, 'annotation file format {} not supported'.format(type(dataset))
+            with open(annotation_file, 'r') as json_file:
+                dataset = json.load(json_file)
+
+            assert type(dataset) == dict, 'annotation file format {} not supported'.format(type(dataset))
             print('Done (t={:0.2f}s)'.format(time.time() - tic))
             self.dataset = dataset
             self.createIndex()
