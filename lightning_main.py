e1e892b3ec6af9180e5a6a25744315c77344e0b5
Sara Naseri Golestani <sarah.gln.97@gmail.com>
2022-08-31 11:37:58 -0600

    final changes for final runs

--- lightning_main.py
+++ lightning_main.py
@@ -14,7 +14,7 @@ import numpy as np
 import random
 from  pytorch_lightning import seed_everything
 from pytorch_lightning.callbacks import LearningRateMonitor
-
+import wandb
 warnings.filterwarnings('always')
 
 # torch.backends.cudnn.benchmark = True
@@ -25,18 +25,18 @@ parser.add_argument('--save_path', default='saved_models')
 parser.add_argument('--num_classes', type=int, default=80)
 parser.add_argument('--input_size', type=int, default=224)
 parser.add_argument('--val_zoom_factor', type=int, default=0.875)
-parser.add_argument('--batch_size', type=int, default=5)
+parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--num_epochs', type=int, default=200)
 parser.add_argument('--num_workers', type=int, default=1)
 parser.add_argument('--max_epochs', type=int, default=300)
-parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--lr', type=float, default=5e-4)
 parser.add_argument('--num_gpu', type=int, default=1)
 parser.add_argument('--num_nodes', type=int, default=1)
-parser.add_argument('--dataset_sampling_ratio', default=0.3, type=float, help="sampling ratio of dataset")
+parser.add_argument('--dataset_sampling_ratio', default=1, type=float, help="sampling ratio of dataset")
 parser.add_argument('--seed', default=0, type=int, help="seed for randomness")
 parser.add_argument('--wandb_name', default='resnet_uav_mgpu')
 parser.add_argument('--load_from_chkp', default=False, type=bool, help="load from check point")
-parser.add_argument('--train', default=False, type=bool, help="load from check point")
+parser.add_argument('--train', default=True, type=bool, help="load from check point")
 parser.add_argument('--num_devices', type=int, default=2)
 
 
@@ -65,7 +65,8 @@ def run():
 if __name__ == '__main__':
     args = parser.parse_args()
     lr_monitor = LearningRateMonitor(logging_interval='step')
-    wandb_logger = WandbLogger(project=args.wandb_name, entity="sara_ngln")
+
+    wandb_logger = WandbLogger(project='resnet-18', name=args.wandb_name, entity="sara_ngln")
 
     wandb_logger.experiment.config.update({
         "val_zoom_factor": args.val_zoom_factor,
@@ -74,22 +75,20 @@ if __name__ == '__main__':
         "num_nodes": args.max_epochs,
         "dataset_sampling_ratio": args.dataset_sampling_ratio,
     })
+
     set_seed(args.seed)
     run()
     model = ResNet18(args)
     trainer = pl.Trainer(logger=wandb_logger, callbacks=[checkpoint_callback, lr_monitor], max_epochs=args.num_epochs,
                          num_nodes=args.num_nodes, gpus=args.num_gpu, accelerator="gpu", devices=args.num_devices, precision=32,
                          strategy='ddp')
-    # train_dl = COCODatasetLightning(args).train_dataloader()
-    # val_dl = COCODatasetLightning(args).val_dataloader()
-    # test_dl = COCODatasetLightning(args).test_dataloader()
+    train_dl = COCODatasetLightning(args).train_dataloader()
+    val_dl = COCODatasetLightning(args).val_dataloader()
+    # train_dl = UAVDatasetLightning(args).train_dataloader()
+    # val_dl = UAVDatasetLightning(args).val_dataloader()
     if args.load_from_chkp and args.train:
-        train_dl = UAVDatasetLightning(args).train_dataloader()
-        val_dl = UAVDatasetLightning(args).val_dataloader()
         trainer.fit(model, train_dl, val_dl, ckpt_path=os.path.join(args.save_path, args.checkpoint_name))
     elif args.train:
-        train_dl = UAVDatasetLightning(args).train_dataloader()
-        val_dl = UAVDatasetLightning(args).val_dataloader()
         trainer.fit(model, train_dl, val_dl)
 
     if not args.train:
